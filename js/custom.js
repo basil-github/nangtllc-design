cde182da6c936e395363cc9e5a3efbf5ecc34b93
Basil <basil7here+codegreen@gmail.com>
2022-12-17 13:34:05 +0530

    footer

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+$(function () {
+  // MENU
+  $(".navbar-collapse a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // AOS ANIMATION
+  AOS.init({
+    disable: "mobile",
+    duration: 800,
+    anchorPlacement: "center-bottom",
+  });
+
+  // SMOOTHSCROLL NAVBAR
+  $(function () {
+    $(".navbar a, .hero-text a").on("click", function (event) {
+      var $anchor = $(this);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top - 49,
+          },
+          1000
+        );
+      event.preventDefault();
+    });
+  });
+  $(function () {
+    $(document).scroll(function () {
+      if ($(this).scrollTop() >= $(".header__root").offset().top - 49) {
+        $(".header__root").css("background", "red");
+      } else {
+        $(".header__root").css("background", "orange");
+      }
+    });
+  });
+});
