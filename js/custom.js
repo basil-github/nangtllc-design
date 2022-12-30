0103e78c5db555e2a4e08493b6f04a99ee04f3be
Basil <basil7here+codegreen@gmail.com>
2022-12-30 14:10:05 +0530

    hero done

--- js/custom.js
+++ js/custom.js
@@ -112,9 +112,9 @@ $(function () {
         // $(".header__level-one").css({ "background-color": "#ffffff" })
         $(".header__root").css({ "opacity": "1", "height": "100%", "pointer-events": "auto" })
       } else {
-        $(".header__outer").css({ "background-color": "#5e59596b" })
-        // $(".header__inner").css({ "background-color": "#5e59596b" })
-        // $(".header__level-one").css({ "background-color": "#5e59596b", "pointer-events": "auto", })
+        $(".header__outer").css({ "background-color": "#ffffff6b" })
+        // $(".header__inner").css({ "background-color": "#ffffff6b" })
+        // $(".header__level-one").css({ "background-color": "#ffffff6b", "pointer-events": "auto", })
         $(".header__root").css({ "opacity": "0", "height": "0", "pointer-events": "none", })
       }
       if (screen.width > 962) {
